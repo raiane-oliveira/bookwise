602ba8c88f6bc55593f76dfa2a842e9352449372
raiane-oliveira <raiane.oliveira404@gmail.com>
2023-10-07 13:47:41 -0300

    chore: configure lint, tailwind and fonts

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Nunito_Sans as NunitoSans } from 'next/font/google'
+
+const nunitoSans = NunitoSans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito-sans',
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${nunitoSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
